9ccc661e6286829325cd7a9f3b05e799040350ac
Dmitry-V-Makarov <dm.v.makarov@gmail.com>
2020-11-23 09:39:17 +0300

    HW 1.5 redone

--- sql-23/hw/1.5.hw.sql
+++ sql-23/hw/1.5.hw.sql
@@ -4,22 +4,31 @@ SELECT customer_id, rental_id, ROW_NUMBER() OVER (PARTITION BY customer_id ORDER
 FROM rental;
 
 -- Assignment 2
--- EXPLAIN ANALYZE
+EXPLAIN ANALYZE
 -- CREATE MATERIALIZED VIEW n_rented AS
 SELECT first_name, last_name, COUNT(sub.special_features) n_films
 FROM (
 SELECT c.customer_id, c.first_name, c.last_name, f.film_id, f.special_features 
 FROM customer c
-LEFT JOIN rental r ON c.customer_id = r.customer_id 
-LEFT JOIN inventory i ON r.rental_id = i.inventory_id 
-LEFT JOIN film f ON f.film_id = i.film_id
-WHERE 'Behind the Scenes' = ANY(f.special_features) -- Option 1
--- WHERE f.title IS NOT NULL AND array_to_string(f.special_features, ',') like '%Behind the Scenes%' -- Option 2
--- Option 3 would be to unnest special_features /not optimal/
+INNER JOIN rental r ON c.customer_id = r.customer_id 
+INNER JOIN inventory i ON r.inventory_id = i.inventory_id 
+INNER JOIN film f ON f.film_id = i.film_id
+WHERE 'Behind the Scenes' = ANY(f.special_features)
+-- WHERE f.title IS NOT NULL AND array_to_string(f.special_features, ',') like '%Behind the Scenes%'
+-- WHERE 'Behind the Scenes' IN (SELECT unnest(f.special_features))
 ) sub
-GROUP BY customer_id, first_name, last_name
+GROUP BY customer_id, 1, 2
 ORDER BY n_films DESC;
 -- WITH NO DATA;
+
+CREATE INDEX public_customer_id ON customer(customer_id);
+DROP INDEX public_customer_id;
+CREATE INDEX public_film_id ON film(film_id);
+DROP INDEX public_film_id;
+CREATE INDEX public_rental_id ON rental(inventory_id, customer_id);
+DROP INDEX public_rental_id;
+
+CREATE INDEX film_index ON film USING GIN(special_features);
 
 REFRESH MATERIALIZED VIEW n_rented;
 
