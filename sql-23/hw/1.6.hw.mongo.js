23836319c87b1ba477617747a363a3f995dbec4e
Dmitry-V-Makarov <dm.v.makarov@gmail.com>
2020-11-23 16:45:17 +0300

    1.6 HW continue

--- sql-23/hw/1.6.hw.mongo.js
+++ sql-23/hw/1.6.hw.mongo.js
@@ -3,14 +3,14 @@
 // Main assignment
 use test
 
-// db.users.insert({first_name : "Alex", last_name: "Smith", contacts : {p1 : "+1 494 9494", p2 : "+1 202 3234", address : "23, Clear road"}})
-// db.users.insert({first_name : "Mary", last_name: "Jones", contacts : {p1 : "+1 423 9234", p2 : "+1 202 1876", fax : "+1 490 2890", address : "11, Fifth Avenue"}})
+db.users.insert({first_name : "Alex", last_name: "Smith", contacts : {p1 : "+1 494 9494", p2 : "+1 202 3234", address : "23, Clear road"}})
+db.users.insert({first_name : "Mary", last_name: "Jones", contacts : {p1 : "+1 423 9234", p2 : "+1 202 1876", fax : "+1 490 2890", address : "11, Fifth Avenue"}})
 
 db.users.find()
 
 
 // Additional assignment
-/* db.test.aggregate([
+db.test.aggregate([
 {
 $project: 
 {
@@ -19,5 +19,4 @@ contacts: {
 $objectToArray: "contacts" 
 }
 }
-}]) 
-*/
+}])
